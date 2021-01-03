778ccfcec627d3da05aa21ca6a500112e7799cb1
David Valdivia <d.valdiviamartinez@gmail.com>
2021-01-03 16:04:38 +0100

    Frog moving (not well enough)

---
+++ Frogger Online/Assets/Scripts/Frog.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class Frog : MonoBehaviour
+{
+    public int movement_space = 5;
+    private Rigidbody2D rb = null;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        //Movement
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+            rb.MovePosition(rb.position + Vector2.up * movement_space);
+
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+            rb.MovePosition(rb.position + Vector2.down * movement_space);
+
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+            rb.MovePosition(rb.position + Vector2.left * movement_space);
+
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+            rb.MovePosition(rb.position + Vector2.right * movement_space);
+    
+    }
+}
