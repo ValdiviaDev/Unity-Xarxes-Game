f335c1626594928dcffbca68865849c15980f20a
Ricardogll <rikilonga@gmail.com>
2021-01-11 00:29:17 +0100

    Frog movement improved and animations added :frog:

--- Frogger Online/Assets/Scripts/Frog.cs
+++ Frogger Online/Assets/Scripts/Frog.cs
@@ -4,11 +4,19 @@ public class Frog : MonoBehaviour
 {
     public int movement_space = 5;
     private Rigidbody2D rb = null;
+    public Animator animator;
 
     public float limit_down = -6.0f;
     public float limit_left = -8.73f;
     public float limit_right = 6.25f;
     public float limit_up = 6.0f;
+
+    public float jump_time = 0.5f;
+    public float dist = 5.0f;
+    public float v_dist = 5.0f;
+
+    private float aux_time = 0.0f;
+    private string direction = "none";
 
     void Start()
     {
@@ -17,18 +25,80 @@ public class Frog : MonoBehaviour
 
     void Update()
     {
-        //Movement
-        if (Input.GetKeyDown(KeyCode.UpArrow) && transform.position.y < limit_up)
-            rb.MovePosition(rb.position + Vector2.up * movement_space);
+        if (aux_time <= 0.0f)
+        {
+            animator.SetFloat("speed", 0.0f);
+            animator.SetFloat("h_speed", 0.0f);
 
-        else if (Input.GetKeyDown(KeyCode.DownArrow) && transform.position.y > limit_down)
-            rb.MovePosition(rb.position + Vector2.down * movement_space);
+            //Movement
+            if (Input.GetKeyDown(KeyCode.UpArrow) && transform.position.y < limit_up)
+            {
+                //rb.MovePosition(rb.position + Vector2.up * movement_space);
+                animator.SetFloat("h_speed", 1);
+                aux_time = jump_time;
+                direction = "up";
+            }
 
-        else if (Input.GetKeyDown(KeyCode.LeftArrow) && transform.position.x > limit_left)
-            rb.MovePosition(rb.position + Vector2.left * movement_space);
+            else if (Input.GetKeyDown(KeyCode.DownArrow) && transform.position.y > limit_down)
+            {
+                //rb.MovePosition(rb.position + Vector2.down * movement_space);
+                animator.SetFloat("h_speed", -1);
+                aux_time = jump_time;
+                direction = "down";
+            }
 
-        else if (Input.GetKeyDown(KeyCode.RightArrow) && transform.position.x < limit_right)
-            rb.MovePosition(rb.position + Vector2.right * movement_space);
-    
+            else if (Input.GetKeyDown(KeyCode.LeftArrow) && transform.position.x > limit_left)
+            {
+                //rb.MovePosition(rb.position + Vector2.left * movement_space);
+                animator.SetFloat("speed", -1);
+
+                aux_time = jump_time;
+                direction = "left";
+
+            }
+
+            else if (Input.GetKeyDown(KeyCode.RightArrow) && transform.position.x < limit_right)
+            {
+                //rb.MovePosition(rb.position + Vector2.right * movement_space);
+                animator.SetFloat("speed", 1);
+                aux_time = jump_time;
+                direction = "right";
+
+            }
+
+        }
+
+        if(aux_time > 0.0f)
+        {
+            aux_time -= Time.deltaTime;
+
+            if (direction == "down")
+            {
+                Vector2 new_pos = rb.position + Vector2.down * v_dist * Time.deltaTime;
+                rb.MovePosition(new Vector2(new_pos.x, Mathf.Clamp(new_pos.y, limit_down, limit_up)));
+            }
+            else if (direction == "up")
+            {
+                Vector2 new_pos = rb.position + Vector2.up * v_dist * Time.deltaTime;
+                rb.MovePosition(new Vector2(new_pos.x, Mathf.Clamp(new_pos.y, limit_down, limit_up)));
+                Debug.Log(new_pos);
+            }
+            else if (direction == "left")
+            {
+                Vector2 new_pos = rb.position + Vector2.left * dist * Time.deltaTime;
+                rb.MovePosition(new Vector2(Mathf.Clamp(new_pos.x, limit_left, limit_right), new_pos.y));
+            }
+            else if (direction == "right")
+            {
+                Vector2 new_pos = rb.position + Vector2.right * dist * Time.deltaTime;
+                rb.MovePosition(new Vector2(Mathf.Clamp(new_pos.x, limit_left, limit_right), new_pos.y));
+            }
+            
+
+            if (aux_time <= 0.0f)
+                direction = "none";
+        }
+
+     
     }
 }
