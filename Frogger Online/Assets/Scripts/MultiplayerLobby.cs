f05fdf17eaaf8fdcb08f2cf90308cc6f18aa10cd
Julià Mauri Costa <julimacowork@gmail.com>
2021-01-27 15:49:48 +0100

    Private room when using ID :clock9:

--- Frogger Online/Assets/Scripts/MultiplayerLobby.cs
+++ Frogger Online/Assets/Scripts/MultiplayerLobby.cs
@@ -92,7 +92,7 @@ namespace Com.Cotxe11.FroggerOnline
             {
                 if(roomIDLabel.text.Length > 0)
                 {
-                    PhotonNetwork.JoinOrCreateRoom(roomIDLabel.text, new RoomOptions { MaxPlayers = maxPlayersPerRoom }, TypedLobby.Default);
+                    PhotonNetwork.JoinOrCreateRoom(roomIDLabel.text, new RoomOptions { MaxPlayers = maxPlayersPerRoom, IsVisible = false }, TypedLobby.Default);
                 }
                 else
                 {
